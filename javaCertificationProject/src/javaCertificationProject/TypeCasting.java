48fa9f30cc867051021b1b101413931a4e7c2bc2
kriti693 <74588359+kriti693@users.noreply.github.com>
2022-04-05 18:45:14 +0530

    Add files via upload
    Udemy Module 1,2,3,4 Completed

---
+++ javaCertificationProject/src/javaCertificationProject/TypeCasting.java
@@ -0,0 +1,103 @@
+package javaCertificationProject;
+
+import java.util.Scanner;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+
+//		int ditance; // define variable
+//		ditance=4750;
+//		int km,m;
+//		km=ditance/1000;
+//		//km=ditance/;
+//		m=ditance%1000;
+//		System.out.println("output of ditance "+km);
+//		System.out.println("output of km "+m);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//		long time = 4000000000L;
+//		long devide = 1000000000L;
+//		int second;
+//		second=(int) (time/devide);
+//		System.out.println("out put of second "+second);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//		float f=10.45f/9.5f;
+//		System.out.println(f);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//		long data=(3*5)%7;
+//		long data1=3*5%7;
+//		long data2=(3+5)%7;
+//		long data3=6+5*9+5;
+//		System.out.println(data2);
+//		System.out.println(data3);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//		boolean isBlond = false;
+//		boolean isGraduated = false;
+//		boolean isfemale = false;
+//		boolean result = (isfemale && isGraduated && !isBlond) || (!isfemale && isGraduated && isBlond);
+//		System.out.println(result);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		
+
+		
+
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter the no");
+//		 int grade=sc.nextInt();
+//		 if(grade>0&&grade<60) {
+//			 if(grade>0&&grade<30) {
+//				 System.out.println("D"+grade);
+//			 }
+//			 else if(grade>30&&grade<50) {
+//				 System.out.println("C"+grade); 
+//			 }
+//			 else if(grade>50&&grade<60) {
+//				 System.out.println("Your grade is B "+grade);
+//			 }
+//			 System.out.println("You may re-attend the exam");
+//		 }
+//		 else if(grade>60&&grade<100){
+//			 if(grade>60&&grade<80) {
+//				 System.out.println("Your grade is B "+grade);
+//			 }
+//			 else {
+//				 System.out.println("Your grade is A "+grade);
+//			 }
+//			 System.out.println("Thanks for attending the exam");
+//		 }
+//		 else {
+//			 System.out.println("wrong input");
+//		 }
+//		  
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//		 for(int i=10;i>0;i--) {
+//			 for(int j=i-1;j>0;j--) {
+//				 System.out.print("*"); 
+//				  
+//			 }
+//			 System.out.println("");
+//			 
+//			  
+//		 }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//		for(int i=10;i>0;i--) {
+//			 System.out.print("*");
+//		}
+//		for(int i=10;i>0;i--) {
+//			 System.out.println("*"+"         "+"*");
+//		}
+//		for(int i=11;i>0;i--) {
+//			 System.out.print("*");
+//}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	}
+}
